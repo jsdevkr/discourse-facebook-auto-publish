8495ea8fed68f54a8fd682c56b55d2ffe6697e67
InGrowth <gimdongwoo@gmail.com>
2017-02-17 19:47:00 +0900

    add post user

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ let checkDate = new Date('2017-02-14T00:37:55.088Z');
 function getDiscourse() {
   return new Promise((resolve, reject) => {
     const restClient = new RestClient();
-    restClient.get(process.env.DISCOURSE_URL + '/latest.json').then((results) => {
+    restClient.get(process.env.DISCOURSE_URL + '/top/daily.json').then((results) => {
       if (results && typeof results !== 'object') results = JSON.parse(results);
 
       if (results && results.topic_list && results.topic_list.topics) {
-        return parseTopics(results.topic_list.topics).then(resolve, reject);
+        return parseTopics(results.topic_list.topics, results.users).then(resolve, reject);
       }
       console.error('load error, checkDate not update');
       return reject(error);
@@ -20,7 +20,7 @@ function getDiscourse() {
 }
 
 // parse topics
-function parseTopics(topics) {
+function parseTopics(topics, users) {
   const filteredTopics = topics.filter((info) => {
     const created_at = new Date(info.created_at);
     return created_at > checkDate;
@@ -28,11 +28,20 @@ function parseTopics(topics) {
   console.log('filteredTopics', JSON.stringify(filteredTopics));
   var p = Promise.resolve();
   return filteredTopics.reduce((pacc, info) => {
+    // subject
     const _subject = info.title;
-    const _text = process.env.DISCOURSE_URL + '/t/' + info.id;
+    // find post user
+    const _posters = info.posters.find((poster) => {
+      return !poster.extras;
+    });
+    const _postUser = users.find((user) => {
+      return user.id === _posters.user_id;
+    });
+    // body
+    const _html = 'by @' + _postUser.username + '<br/>' + process.env.DISCOURSE_URL + '/t/' + info.id;
 
     const fn = function () {
-      return emailSender(_subject, _text);
+      return emailSender(_subject, _html);
     };
 
     return pacc = pacc.then(fn);
@@ -40,7 +49,7 @@ function parseTopics(topics) {
 }
 
 // email send
-function emailSender(_subject, _text) {
+function emailSender(_subject, _html) {
   return new Promise((resolve, reject) => {
     // create reusable transporter object using the default SMTP transport
     let transport = Nodemailer.createTransport({
@@ -56,7 +65,7 @@ function emailSender(_subject, _text) {
       from: process.env.EMAIL_FROM,
       to: process.env.EMAIL_TO,
       subject: _subject,
-      text: _text
+      html: _html
     };
 
     console.log('mailOptions', mailOptions);
